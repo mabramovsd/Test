d9d1c714aa900c69b4a15b35edb350d39ccc0ad3
Mikhail Abramov <beavisabra@yandex.ru>
2018-08-17 16:50:22 +0400

    Идеи Вани. Первая версия
    Движется по стрелочкам, правильная рожа, везде 2 ответа

--- test.cpp
+++ test.cpp
@@ -1,77 +1,63 @@
 #include "TXLib.h"
+#include "answer.cpp"
 
-struct Answer
+struct Hero
 {
-    //Обязательные
-    const char* text;
-    bool pravilnyi;
-    //Необязательные
+    int x_start;
+    int y_start;
+    int x;
+    int y;
+    int width;
+    int height;
+    int speed;
     HDC pic;
-    int x1;
-    int y1;
-    int x2;
-    int y2;
 };
 
-void drawAnswer(Answer ans)
+void move_hero(Hero* man)
 {
-    txRectangle(ans.x1, ans.y1, ans.x2, ans.y2);
-    if (ans.pic != nullptr)
+    if (GetAsyncKeyState(VK_LEFT))
     {
-        txBitBlt (txDC(), ans.x1, ans.y1, ans.x2 - ans.x1, ans.y2 - ans.y1, ans.pic, 130, 150);
+        man->x -= man->speed;
     }
-    txDrawText(ans.x1, ans.y1, ans.x2, ans.y2, ans.text);
-}
-
-struct Question
-{
-    //Обязательные
-    const char* text;
-    Answer ans[100];
-    //Необязательные
-    int count_answers;
-};
-
-void drawQuestion(Question que)
-{
-    txSetFillColor(TX_RED);
-    txRectangle(100, 0, 800, 100);
-    txDrawText(100, 0, 800, 100, que.text);
-
-    for (int nomer = 0; nomer < que.count_answers; nomer++)
+    else if (GetAsyncKeyState(VK_RIGHT))
     {
-        drawAnswer(que.ans[nomer]);
+        man->x += man->speed;
+    }
+
+    if (GetAsyncKeyState(VK_UP))
+    {
+        man->y -= man->speed;
+    }
+    else if (GetAsyncKeyState(VK_DOWN))
+    {
+        man->y += man->speed;
     }
 }
 
 int main()
 {
-    txCreateWindow(900, 500);
+    txCreateWindow(900, 600);
 
     Question que[100];
     int count_questions = 0;
     int kolichestvo_pravilnyh = 0;
     char stroka_dlya_kolichestvo_pravilnyh[100];
 
+    Hero face = {450, 600, 450, 600, 140, 190, 4, txLoadImage("Лицо.bmp")};
+
     //Заполняем вопросы нормально
     {
     que[count_questions++] = {"Выберите число",
-        {{"1", true, txLoadImage("Бузова.bmp")},
-         {"Курица", false},
-         {"g", false},
-         {"456", false},
-         {"dfg", false}}
+        {{"1", true},
+         {"Курица", false}}
     };
     que[count_questions++] = {"Это невопрос. Все ответы правильные",
-        {{"Это ответ1", true, que[0].ans[0].pic},
-         {"Это ответ2", true},
-         {"Это ответ3", true}}
+        {{"Это ответ1", true},
+         {"Это ответ2", true}}
     };
     que[count_questions++] = {"Выберите число",
         {{"2", true},
-         {"Вася", false},
-         {"Вася", false},
-         {"15", true}}
+         {"Вася", false}}
     };
     que[count_questions++] = {"Выберите число",
         {{"2", true},
@@ -97,8 +83,8 @@ int main()
             //900 - ширина экрана
             int shirina_otveta = 900 / que[nomer].count_answers;
 
-            que[nomer].ans[otvet].x1 = 10 + otvet * shirina_otveta;
-            que[nomer].ans[otvet].x2 = que[nomer].ans[otvet].x1 + shirina_otveta - 20;
+            que[nomer].ans[otvet].x1 = 100 + otvet * shirina_otveta;
+            que[nomer].ans[otvet].x2 = que[nomer].ans[otvet].x1 + shirina_otveta - 200;
 
             que[nomer].ans[otvet].y1 = 300;
             que[nomer].ans[otvet].y2 = 400;
@@ -114,6 +100,11 @@ int main()
         txClear();
         drawQuestion(que[nomer_voprosa]);
 
+        txBitBlt (txDC(), face.x - face.width / 2, face.y - face.height / 2, face.width, face.height, face.pic);
+
+        move_hero(&face);
+
+
         sprintf(stroka_dlya_kolichestvo_pravilnyh,
             "Количество правильных ответов %d из %d",
             kolichestvo_pravilnyh,
@@ -122,27 +113,19 @@ int main()
 
         for (int nomer = 0; nomer < que[nomer_voprosa].count_answers; nomer++)
         {
-            if (
-                txMouseX() >= que[nomer_voprosa].ans[nomer].x1 and
-                txMouseX() <= que[nomer_voprosa].ans[nomer].x2 and
-                txMouseY() >= que[nomer_voprosa].ans[nomer].y1 and
-                txMouseY() <= que[nomer_voprosa].ans[nomer].y2
-            )
+            if (face.x >= que[nomer_voprosa].ans[nomer].x1 and
+                face.x <= que[nomer_voprosa].ans[nomer].x2 and
+                face.y >= que[nomer_voprosa].ans[nomer].y1 and
+                face.y <= que[nomer_voprosa].ans[nomer].y2)
             {
-                txSetFillColor(TX_YELLOW);
-                txFloodFill(que[nomer_voprosa].ans[nomer].x1 + 10,
-                            que[nomer_voprosa].ans[nomer].y1 + 10);
-                txSleep(10);
-
-                if (txMouseButtons() & 1)
+                if (que[nomer_voprosa].ans[nomer].pravilnyi)
                 {
-                    if (que[nomer_voprosa].ans[nomer].pravilnyi)
-                    {
-                        kolichestvo_pravilnyh = kolichestvo_pravilnyh + 1;
-                    }
-                    nomer_voprosa = nomer_voprosa + 1;
-                    txSleep(1000);
+                    kolichestvo_pravilnyh = kolichestvo_pravilnyh + 1;
                 }
+
+                nomer_voprosa = nomer_voprosa + 1;
+                face.x = face.x_start;
+                face.y = face.y_start;
             }
         }
 
@@ -169,6 +152,8 @@ int main()
         txTextOut(30, 400, "Вы идиот");
     }
     }
+
+    txDeleteDC(face.pic);
 
     return 0;
 }
